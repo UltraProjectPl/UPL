0f9f46aa4c889ae0dc53a797cfdc95a0d1c2e036
Jakub Sumara <jakub.sumara@minebest.com>
2020-10-31 16:57:23 +0100

    Loaded file

--- main.cpp
+++ main.cpp
@@ -1,2 +1,27 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+std::string loadFile(const std::string &fileName);
+
 int main() {
+    std::string fileSrc = "/home/ultra/Projects/UPL/ExampleApp/index.upl";
+    auto fileContent = loadFile(fileSrc);
+
+    std::cout << fileContent << std::endl;
 }
+
+std::string loadFile(const std::string &fileName) {
+    std::ifstream file(fileName);
+    std::string fileContent;
+
+    if (file.is_open()) {
+        std::string line;
+        while (std::getline(file, line)) {
+            fileContent += line;
+        }
+        file.close();
+    }
+
+    return fileContent;
+};
